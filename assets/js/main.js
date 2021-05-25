43c30c4c4c389c190dbf3de7e5a365629479a256
Bhanu <salus-sage@users.noreply.github.com>
2021-05-25 22:17:26 +0530

    add scroll to top function

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,12 @@ if (menuToggle) {
     }
   }, true);
 }
+
+// scroll to top of the page
+scrollToTop = function () {
+  var c = document.documentElement.scrollTop || document.body.scrollTop;
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, c - c / 8);
+  }
+};
